56d141e479d7adfabe0d80e0a53224d38b23127a
pplnostrateam <insantaninostra@gmail.com>
2016-03-23 13:25:58 +0700

    Init empty spring boot project

---
+++ src/main/java/com/backend/insantani/BackendDevelopmentApplication.java
@@ -0,0 +1,12 @@
+package com.backend.insantani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDevelopmentApplication.class, args);
+	}
+}
