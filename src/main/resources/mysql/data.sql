83c15fb08a25e7fcf7fb21d489ea60bc0e39c06a
pplnostrateam <insantaninostra@gmail.com>
2016-03-23 23:08:42 +0700

    Delete data.sql

--- src/main/resources/mysql/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Vegetable (name,size) VALUES ('Carrot',1);
-INSERT INTO Vegetable (name,size) VALUES ('Carrot',2);
-INSERT INTO Vegetable (name,size) VALUES ('Carrot',3);
-INSERT INTO Vegetable (name,size) VALUES ('Carrot',4);
-
